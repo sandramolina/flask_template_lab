7a0ef6a9460baedb91526c44def2cdfc91fdaedc
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-14 13:58:28 +0100

    did the add event functionality

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,5 +6,13 @@ from models.event_list import *
 def home():
     return render_template("index.html", title="Home", events = event_list)
 
-# @app.route('/addevent'):
-# def add_event():
+@app.route('/addevent', methods=['POST'])
+def add_event():
+    event_date = request.form["date"]
+    event_name = request.form["event_name"]
+    guest_number = request.form["guest_number"]
+    event_address = request.form["address"]
+    event_description = request.form["description"]
+    new_event = Event(event_date, event_name, guest_number, event_address, event_description)
+    add_new_event(new_event)
+    return render_template("index.html", title="Home", events = event_list)
